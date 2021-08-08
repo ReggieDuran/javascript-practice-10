fa38983370a29e0cd4bbd389e41d839e82542c92
Reggie <reggieduran15@gmail.com>
2021-08-08 13:35:07 +0800

    async await

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -1,6 +1,21 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition( 
+      success => {
+        resolve(success);
+      },
+      error => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return promise;
+}
+
 const setTimer = (duration) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -10,19 +25,29 @@ const setTimer = (duration) => {
   return promise;
 };
 
-function trackUserHandler() {
-  console.log('Clicked!');
-  navigator.geolocation.getCurrentPosition(posData => {
-    setTimer(2000).then(data => {
-      console.log(data, posData);
-    });
-  }, error => {
-    console.log(error);
-  });
-  setTimer(1000).then(() => {
-    console.log('Timer done!');
-  });
-  console.log('Getting position...');
+async function trackUserHandler() {
+  const posData = await getPosition();
+  const timerData = await setTimer(2000);
+  console.log(posData, timerData);
+  // console.log('Clicked!');
+  // let positionData;
+  // getPosition()
+  //   .then(posData => {
+  //     positionData = posData;
+  //     return setTimer()
+  //   })
+  //   .catch( err => {
+  //     console.log(err);
+  //     return 'on we go...';
+  //   })
+  //   .then(data => {
+  //     console.log(data, positionData)
+  //   });
+ 
+  // setTimer(1000).then(() => {
+  //   console.log('Timer done!');
+  // });
+  // console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
