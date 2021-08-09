3506e123c78583ff95197b29ca106e4fca62d430
Reggie <reggieduran15@gmail.com>
2021-08-09 11:42:36 +0800

    promise race/all/allSettled

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -26,8 +26,17 @@ const setTimer = (duration) => {
 };
 
 async function trackUserHandler() {
-  const posData = await getPosition();
-  const timerData = await setTimer(2000);
+  let positionData;
+  let posData;
+  let timerData;
+
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+  
   console.log(posData, timerData);
   // console.log('Clicked!');
   // let positionData;
@@ -44,13 +53,37 @@ async function trackUserHandler() {
   //     console.log(data, positionData)
   //   });
  
-  // setTimer(1000).then(() => {
-  //   console.log('Timer done!');
-  // });
-  // console.log('Getting position...');
+  setTimer(1000).then(() => {
+    console.log('Timer done!');
+  });
+  console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
+
+
+// Promise.race([
+//   getPosition(),
+//   setTimer(1000)
+// ]).then(data => {
+//   console.log(data);
+// });
+
+// Promise.all([
+//   getPosition(),
+//   setTimer(1000)
+// ]).then(promiseData => {
+//   console.log(promiseData);
+// });
+
+Promise.allSettled([
+  getPosition(),
+  setTimer(1000)
+]).then(promiseData => {
+  console.log(promiseData);
+});
+
+
 
 // let result = 0;
 
